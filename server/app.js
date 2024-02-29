1a5347a1f4473831159498f15c2ff70d72055014
Ricardo Antonio Rivera Cotes <barkhitordtroll@gmail.com>
2024-02-28 20:00:21 -0400

    configurando cosas

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json('funcionando bro')
+})
+
 app.listen(3000, () => {
   console.log('Click Here brou http://localhost:3000')
 })
