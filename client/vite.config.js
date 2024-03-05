7a72ec7dbbd77acc7f17ee1e8f06880b761e1722
Ricardo Antonio Rivera Cotes <barkhitordtroll@gmail.com>
2024-03-05 18:17:01 -0400

    probando

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react({
+    // Configuraciones adicionales del plugin React aquí
+    // Por ejemplo, para habilitar el Fast Refresh (recarga rápida) en desarrollo:
+    fastRefresh: true,
+  })],
+  // Configuraciones adicionales de Vite aquí
+  // Por ejemplo, para configurar el directorio base de tu proyecto:
+  base: './',
+  // Para definir variables de entorno y otros ajustes específicos:
+  server: {
+    host: true,
+    port: 5000 // Define el puerto en el que se iniciará el servidor de desarrollo
+  },
+  build: {
+    // Configuraciones específicas de construcción aquí
+  },
+});
