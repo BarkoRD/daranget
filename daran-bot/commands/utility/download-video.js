5c5526b6c92e6ccade17c81e5ade196433a115ce
Adolfo Rosa <adolfoklk0307@gmail.com>
2024-03-17 15:35:17 -0400

    solving minimal errors

--- daran-bot/commands/utility/download-video.js
+++ daran-bot/commands/utility/download-video.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder } = require('discord.js')
-const axios = require('axios')
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,16 +14,22 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    const url = interaction.options.getString('url')
-    const endpoint = `https://api.daranhub.com/download`
-    const { data } = await axios.post(endpoint, { url })
-    if (data.url) {
-      await interaction.reply({ content: data.url, ephemeral: true })
-    } else {
-      await interaction.reply({
-        content: 'No se pudo descargar el video',
-        ephemeral: true,
-      })
+    const url = interaction.options.getString('url');
+    const endpoint = 'https://api.daranhub.com/download';
+    try {
+      const { data } = await axios.post(endpoint, JSON.stringify({ url }), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (data.url) {
+        await interaction.reply({ content: data.url, ephemeral: true });
+      } else {
+        await interaction.reply({ content: 'No se pudo descargar el video', ephemeral: true });
+      }
+    } catch (error) {
+      console.error('Error al descargar el video:', error);
+      await interaction.reply({ content: 'Ocurrió un error al descargar el video', ephemeral: true });
     }
   },
-}
+};
